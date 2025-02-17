1d88596d5bd1d0fbcd9e76f98273a770d3bfc3aa
fanhuanxi <2290991129@qq.com>
2025-02-18 00:06:36 +0800

    hello java

---
+++ hello.java
@@ -0,0 +1,15 @@
+ class Hello {
+    public static void main(String[] args) {
+        // 换行输出
+        System.out.println("Hello World");
+    }
+ }
+
+ /*
+
+  */
+ class HelloHuanxi {
+    public static void main(String[] args) {
+        System.out.println("Hello Huanxi");
+    }
+ }
