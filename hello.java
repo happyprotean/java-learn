f4ab0e42039fa19f63e580f79ccab02d0a75f728
“fanhuanxi” <fanhuanxi@meituan.com>
2025-02-25 17:40:17 +0800

    feat: 数据类型&流程控制完成

--- hello.java
+++ hello.java
@@ -1,7 +1,16 @@
- class Hello {
+import java.util.Arrays;
+
+class Hello {
     public static void main(String[] args) {
         // 换行输出
         System.out.println("Hello World");
+        int[] arr = new int[10];
+        arr[0] = 1;
+        System.out.println(Arrays.toString(arr));
+        final String str = "hello";
+        System.out.println(str);
+        String res = str.length() == 5 ? "5" : "not 5";
+        System.out.println(res);
     }
  }
 
--- out/production/java-learn/README.md
+++ out/production/java-learn/README.md
@@ -1,2 +1,11 @@
 # java-learn
 Learn Java
+
+final 用于定义常量，常量不可改变（可以定义时赋值，或者定义后单独赋值）
+
+字符串的比较
+- str1 == str2: 用于判断是否为同一个字符串对象，本质是字符串指针比较
+- str1.equals(str2): 用于判断字符串值是否一样
+
+数组
+- 定义：int[] arr = new Array
