280641985cf194e848b844098285f307b8ad6493
“fanhuanxi” <fanhuanxi@meituan.com>
2025-02-27 19:46:09 +0800

    feat: 注解完成

--- com/huanxi/Person.java
+++ com/huanxi/Person.java
@@ -30,10 +30,12 @@ public class Person {
     public int getAge() {
         return this.age;
     }
+    @Deprecated
     public void setAge(int age) {
         this.age = age;
     }
 
+    @Override
     public String toString() {
         return "Person [id=" + this.id + ", name=" + this.name + ", age=" + this.age + "]";
     }
